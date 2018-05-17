7a34569ea14d75e3e1c9bcbbbebc8b1d5d4a6c36
Francois Botha <botha.francois@gmail.com>
2018-05-17 20:46:59 +0200

    Initial Commit

---
+++ src/main/java/io/francoisbotha/sparkbuilder/SparkbuilderApplication.java
@@ -0,0 +1,18 @@
+package io.francoisbotha.sparkbuilder;
+
+import io.francoisbotha.sparkbuilder.sc.GreetingInScala;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkbuilderApplication {
+
+    public static void main(String[] args) {
+
+        GreetingInScala greetingInScala = new GreetingInScala();
+
+        SpringApplication.run(SparkbuilderApplication.class, args);
+
+        System.out.println(greetingInScala.greet());
+    }
+}
---
+++ src/main/scala/io/francoisbotha/sparkbuilder/sc/GreetingInScala.scala
@@ -0,0 +1,8 @@
+package io.francoisbotha.sparkbuilder.sc
+
+class GreetingInScala {
+  def greet(): String = {
+    val greeting = "Hello from SCALA!!!!"
+    greeting
+  }
+}

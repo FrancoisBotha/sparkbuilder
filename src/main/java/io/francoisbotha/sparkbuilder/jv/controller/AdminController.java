98e27b3b8d7adf90a65ec19fbf9ada0b03bea323
Francois Botha <botha.francois@gmail.com>
2018-05-20 21:38:24 +0200

    KeyCloak Spring Boot Integration

--- src/main/java/io/francoisbotha/sparkbuilder/jv/controller/AdminController.java
+++ src/main/java/io/francoisbotha/sparkbuilder/jv/controller/AdminController.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping("/ui/admin")
     public ModelAndView getAdminPage() {
         ModelAndView modelAndView = new ModelAndView("admin");
         return modelAndView;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
     }
 
 }
